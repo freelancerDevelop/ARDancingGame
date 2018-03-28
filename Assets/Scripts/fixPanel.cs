4df133983f705b001cc1a085fce2cb9df12900e2
Defetysta <T.n.95@interia.pl>
2018-03-28 14:20:36 +0200

    Starting menu; Image recognition

---
+++ Assets/Scripts/fixPanel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Vuforia;
+
+public class fixPanel : MonoBehaviour {
+
+    
+    public GameObject refreshButton;
+    public GameObject cameraButton;
+    private string s = "";
+    private bool flashEnabled = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(DefaultTrackableEventHandler.trackingFound == true)
+        {
+            cameraButton.SetActive(false);
+        }
+
+
+	}
+
+
+    public void OnClickRefresh()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    
+
+    public void ToggleFlash()
+    {
+        flashEnabled = !flashEnabled;
+        CameraDevice.Instance.SetFlashTorchMode(flashEnabled);
+    }
+}
---
+++ Assets/Scripts/gameController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameController : MonoBehaviour {
+
+    public GameObject HSPanel;
+    private Animator anim;
+
+    // Use this for initialization
+    void Start () {
+       anim = HSPanel.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("UDT_Sample");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void OnClickHighscore()
+    {
+        anim.SetTrigger("playHSAnim");
+    }
+
+    public void OnClickBackHighscore()
+    {
+        anim.SetTrigger("goBack");
+    }
+
+}
